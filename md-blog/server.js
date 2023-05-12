22c76bb227aa13b438bc8ce13c2c0f4ec8a1d51d
ntr0pie <tripathi.shreeshv@gmail.com>
2023-05-12 17:15:51 +0530

    Add homepage template

--- md-blog/server.js
+++ md-blog/server.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const app = express();
 const port = process.env.PROCESS || 3000;
+app.set('view engine', 'ejs');
+
+// Routes
+app.use('/articles', require('./routes/articles')); // Articles
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    const articles = [
+    {
+        title: 'Test Article',
+        createdAt: new Date(),
+        description: 'Test description',
+    },
+    {
+        title: 'Test Article 1',
+        createdAt: new Date(),
+        description: 'Test description 1',
+    },];
+    res.render('index', {
+        articles: articles,
+    });
 })
+
 app.listen(port, () => {
     console.log(`[Server] Listening to port ${port}`);
 })
