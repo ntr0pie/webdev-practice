107b254cdaf47cb3816996df86e116cb98671b52
ntr0pie <tripathi.shreeshv@gmail.com>
2023-06-10 03:30:07 +0530

    Chewing glass using vanilla js

--- leetcode-frontend/js/script.js
+++ leetcode-frontend/js/script.js
@@ -2,9 +2,13 @@ const problems = [
     {
         title: '201. Lorem Ipsum',
         dfc: 'Medium',
-        ar8: '54%' 
+        ar8: '54%',
     },
-
+    {
+        title: '101 Lorem Ipsum',
+        dfc: 'Hard',
+        ar8: '73%'
+    },
     {
         title: '101 Lorem Ipsum',
         dfc: 'Hard',
@@ -12,11 +16,19 @@ const problems = [
     }
 ];
 
-
-function createChildEl(child){
-    const tableTag = document.createElement('table');
-}
-
-function renderProblems(){
+function renderProblems() {
     const parentEl = document.getElementById('problems-table');
+    const tableTag = document.createElement('table');
+    for (let p in problems) {
+        const trTag = document.createElement('tr');
+        for (let k in problems[p]) {
+            const tdTag = document.createElement('td');
+            tdTag.innerHTML = problems[p][k];
+            trTag.appendChild(tdTag);
+        }
+        tableTag.appendChild(trTag);
+    }
+    parentEl.appendChild(tableTag);
 }
+
+renderProblems();
