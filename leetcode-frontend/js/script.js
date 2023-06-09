ff8c1042e64a484b7e55a8f6ea350578faf9cfc9
ntr0pie <tripathi.shreeshv@gmail.com>
2023-06-10 03:58:40 +0530

    Add pagination

--- leetcode-frontend/js/script.js
+++ leetcode-frontend/js/script.js
@@ -1,4 +1,4 @@
-const problems = [
+const problems1 = [
     {
         title: '201. Lorem Ipsum',
         dfc: 'Medium',
@@ -16,14 +16,33 @@ const problems = [
     }
 ];
 
-function renderProblems() {
+const problems2 = [
+    {
+        title: '201. Lorem Ipsum',
+        dfc: 'Medium',
+        ar8: '54%',
+    },
+    {
+        title: '101 Lorem Ipsum',
+        dfc: 'Hard',
+        ar8: '71%'
+    },
+    {
+        title: '101 Lorem Ipsum',
+        dfc: 'Hard',
+        ar8: '70%'
+    }
+];
+
+function renderProblems1() {
     const parentEl = document.getElementById('problems-table');
+    parentEl.innerHTML = null;
     const tableTag = document.createElement('table');
-    for (let p in problems) {
+    for (let p in problems1) {
         const trTag = document.createElement('tr');
-        for (let k in problems[p]) {
+        for (let k in problems1[p]) {
             const tdTag = document.createElement('td');
-            tdTag.innerHTML = problems[p][k];
+            tdTag.innerHTML = problems1[p][k];
             trTag.appendChild(tdTag);
         }
         tableTag.appendChild(trTag);
@@ -31,4 +50,20 @@ function renderProblems() {
     parentEl.appendChild(tableTag);
 }
 
-renderProblems();
+function renderProblems2() {
+    const parentEl = document.getElementById('problems-table');
+    parentEl.innerHTML = null;
+    const tableTag = document.createElement('table');
+    for (let p in problems2) {
+        const trTag = document.createElement('tr');
+        for (let k in problems2[p]) {
+            const tdTag = document.createElement('td');
+            tdTag.innerHTML = problems2[p][k];
+            trTag.appendChild(tdTag);
+        }
+        tableTag.appendChild(trTag);
+    }
+    parentEl.appendChild(tableTag);
+}
+
+// renderProblems1();
