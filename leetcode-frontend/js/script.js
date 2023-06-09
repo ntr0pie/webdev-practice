3776ae6f6478daa90f242c515564dc392261914e
ntr0pie <tripathi.shreeshv@gmail.com>
2023-06-10 03:13:05 +0530

    Add hardcoded problem table

---
+++ leetcode-frontend/js/script.js
@@ -0,0 +1,22 @@
+const problems = [
+    {
+        title: '201. Lorem Ipsum',
+        dfc: 'Medium',
+        ar8: '54%' 
+    },
+
+    {
+        title: '101 Lorem Ipsum',
+        dfc: 'Hard',
+        ar8: '73%'
+    }
+];
+
+
+function createChildEl(child){
+    const tableTag = document.createElement('table');
+}
+
+function renderProblems(){
+    const parentEl = document.getElementById('problems-table');
+}
